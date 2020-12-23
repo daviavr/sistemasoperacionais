e1dcd924821d80351dbce4540e0d2ec9a5ff403e
daviavr <64814849+daviavr@users.noreply.github.com>
2020-12-22 22:03:59 -0300

    Add files via upload

---
+++ trab_pronto.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <time.h>
+
+sem_t mutex;
+sem_t acorda_pombo;
+sem_t proximo_usuario;
+
+int contPostIt = 0;
+
+#define N 20
+
+void dorme_aleatorio()
+{
+    sleep(rand() % 3+1);
+}
+
+void leva_mochila_ate_B_e_volta()
+{
+    sleep(rand() % 3+1);
+}
+
+void ColaPostIt()
+{
+    printf("Post-it colado\n");
+}
+
+void *pombo() 
+{
+    while(1)
+    {   sem_wait(&acorda_pombo);  
+        leva_mochila_ate_B_e_volta();     
+        contPostIt = 0;
+        sem_post(&proximo_usuario);     
+        for (int i=0; i<N; i++)     
+        {       
+            printf("Enviando carta %d\n", i+1);     
+        }    
+    }
+}
+
+void *usuario()
+{
+
+    while(1)
+    {
+        sem_wait(&mutex);     
+        dorme_aleatorio();     
+        ColaPostIt();     
+        contPostIt++;     
+        if (contPostIt == N)     
+        {       
+            printf("Mochila cheia\n");   
+            sem_post(&acorda_pombo);
+            sem_wait(&proximo_usuario); 
+        }
+        sem_post(&mutex);   
+    }
+}
+
+
+int main()
+{
+    srand(time(NULL));
+
+    int i = 0;
+    int quantidade_usuarios;
+
+    sem_init(&mutex, 0 , 1);
+    sem_init(&acorda_pombo, 0, 0);
+    sem_init(&proximo_usuario, 0, 0);
+
+    printf("Escolha uma quantidade de usuarios:\n");
+    scanf("%d", &quantidade_usuarios);
+
+    pthread_t *usuarios = malloc(sizeof(pthread_t) * quantidade_usuarios);
+    pthread_t thread_pombo;
+
+    pthread_create(&thread_pombo, NULL, pombo, NULL);
+
+    for(i = 0; i < quantidade_usuarios; i++)
+        pthread_create(&usuarios[i], NULL, usuario, NULL);
+    
+    for(i = 0; i < quantidade_usuarios; i++)
+        pthread_join(usuarios[i],NULL);
+
+    pthread_join(thread_pombo,NULL);
+
+}
